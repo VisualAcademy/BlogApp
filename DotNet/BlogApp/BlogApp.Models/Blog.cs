49c1221806aa80cb7db960a9f67531d6d323a6b0
RedPlus <redplus@redplus.net>
2020-12-29 12:53:52 +0900

    BlogApp.Models

---
+++ DotNet/BlogApp/BlogApp.Models/Blog.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace BlogApp.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Post> Posts { get; set; } = new List<Post>();
+    }
+}
---
+++ DotNet/BlogApp/BlogApp.Models/BlogsContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.Models
+{
+    public class BlogsContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseSqlServer("server=(localdb)\\mssqllocaldb;Database=Blogs"); 
+    }
+}
