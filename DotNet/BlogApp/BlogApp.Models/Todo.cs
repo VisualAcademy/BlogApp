68ad30cb6777299530166d8bd042f95c155fb326
RedPlus <redplus@redplus.net>
2021-09-16 06:07:22 +0900

    Todo

---
+++ DotNet/BlogApp/BlogApp.Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        //[Required]
+        //public string? Title { get; set; }
+        public string Title { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
